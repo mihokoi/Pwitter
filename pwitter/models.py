480172c3403962acc456305548474609e3bd06bf
mihokoi <varthanil12@gmail.com>
2022-08-09 15:40:42 +0200

    safe

--- pwitter/models.py
+++ pwitter/models.py
@@ -1,12 +1,7 @@
-import os.path
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-
-from .validators import FileValidator
 
 
 class Pweet(models.Model):
@@ -14,27 +9,6 @@ class Pweet(models.Model):
                              on_delete=models.DO_NOTHING)
     body = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
-    validate_file = FileValidator(max_size=1024 * 1000,
-                                  content_types=('image/jpeg', 'image/png'))
-    pweet_image = models.FileField(upload_to='pweet_media/',
-                                  validators=[validate_file],
-                                  blank=True)
-    likes = models.ManyToManyField(User, related_name='pweet_posts', blank=True)
-    def __str__(self):
-        return (
-            f"{self.user} "
-            f"({self.created_at:%Y-%m-%d %H:%M}): "
-            f"{self.body[:30]}..."
-        )
-
-
-class PweetReply(models.Model):
-    user = models.ForeignKey(User, related_name="pweets_reply",
-                             on_delete=models.DO_NOTHING)
-    body = models.CharField(max_length=140)
-    created_at = models.DateTimeField(auto_now_add=True)
-    pweet = models.ForeignKey(Pweet, on_delete=models.CASCADE, related_name='replies')
-    likes = models.ManyToManyField(User, related_name='pweet_replies', blank=True)
 
     def __str__(self):
         return (
@@ -42,48 +16,10 @@ class PweetReply(models.Model):
             f"({self.created_at:%Y-%m-%d %H:%M}): "
             f"{self.body[:30]}..."
         )
-
-
-
-class Picture(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    validate_file = FileValidator(max_size=1024 * 1000,
-                                      content_types=('image/jpeg', 'image/png'))
-    picture = models.FileField(upload_to='pweet_media/',
-                                   validators=[validate_file],
-                                   blank=True)
-    pweet = models.OneToOneField(Pweet, on_delete=models.CASCADE, null=True)
-
-
-@receiver(models.signals.post_delete, sender=Picture)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.picture:
-        if os.path.isfile(instance.picture.path):
-            os.remove(instance.picture.path)
-
-
-@receiver(models.signals.pre_save, sender=Picture)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
-    try:
-        old_picture = Picture.objects.get(pk=instance.pk).picture
-    except Picture.DoesNotExist:
-        return False
-
-    new_picture = instance.picture
-    if not old_picture == new_picture:
-        if os.path.isfile(old_picture.path):
-            os.remove(old_picture.path)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    validate_file = FileValidator(max_size=1024*1000,
-                                  content_types=('image/jpeg', 'image/png'))
-    user_image = models.FileField(upload_to='profile_pictures/',
-                                  validators=[validate_file],
-                                  blank=True)
     follows = models.ManyToManyField(
         "self",
         related_name="followed_by",
@@ -100,8 +36,8 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
-        # user_profile.follows.add(instance.profile)
-        user_profile.follows.set([instance.profile.id])
+        user_profile.follows.add(instance.profile)
+        # user_profile.follows.set([instance.profile.id])
         user_profile.save()
 
 
