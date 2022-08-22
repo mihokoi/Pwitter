8e3b6cdff1fb455a9e84610e4bbea8718169657d
mihoko <varthanil12@gmail.com>
2022-08-22 13:12:07 +0200

    linux

--- django_social/settings.py
+++ django_social/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pwitter_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Avarmal12',
+        'USER': 'mihokoi12',
+        'PASSWORD': 'Comiket12',
         'HOST': 'localhost',
         'PORT': '5432',
     }
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pwitter',
+    version='',
+    packages=['pwitter', 'pwitter.migrations', 'django_social'],
+    url='',
+    license='',
+    author='mihoko',
+    author_email='',
+    description=''
+)
